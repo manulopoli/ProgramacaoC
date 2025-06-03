599c3c275e288f1284354a959f5bcc42f2608557
manulopoli <manu.lopes@gmail.com>
2025-06-03 20:58:49 +0000

    praticando mais

--- Praticando3.c
+++ Praticando3.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main () {
-    int numeroSinal = 3000000000; // Este valor excede o limite de um int normal
-    unsigned int numeroSemSinal = 3000000000;
+    unsigned long int numeroGrandePositivo = 4000000000;
+    unsigned int numeroPositivo = 4000000000;
+    long int numeroGrande = 4000000000;
+    int numero = 4000000000;
     
-    printf("Número com sinal: %d\n", numeroSinal);
-    printf("Número sem sinal: %u\n", numeroSemSinal);
+    printf("Número positivo grande: %lu\n", numeroGrandePositivo);
+    printf("Número positivo: %u\n", numeroPositivo);
+    printf("Número grande: %ld\n", numeroGrande);
+    printf("Número: %d\n", numero);
 
     return 0;
 }
