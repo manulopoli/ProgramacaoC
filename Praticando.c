2eb5f82979f23e71ea39884a207270b3c014a7b9
manulopoli <manu.lopes@gmail.com>
2025-06-03 20:07:58 +0000

    Praticando

---
+++ Praticando.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+/*
+Soma (+)
+Subtração (-)
+Multiplicação (*)
+Divisão (/)
+
+*/
+
+int numero1, numero2;
+int soma, subtracao, multiplicacao, divisao;
+
+printf("Entre com o numero 1: \n");
+scanf("%d", &numero1);
+
+printf("Entre com o numero 2: \n");
+scanf("%d", &numero2);
+
+
+    // operacao soma
+soma = numero1 + numero2;
+
+    // operacao subtracao
+subtracao = numero1 - numero2;
+
+    // operacao multiplicacao
+multiplicacao = numero1 * numero2;
+
+    // operacao divisao
+divisao = numero1 / numero2;
+
+printf("A Soma é: %d\n", soma);
+printf("A Subtracao é: %d\n", subtracao);
+printf("A Multiplicacao é: %d\n", multiplicacao);
+printf("A Divisao é: %d\n", divisao);
+
+
+
+}
