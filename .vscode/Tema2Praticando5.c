098dc6d4cd8de16828b929889b6f2b45d3728318
manulopoli <manu.lopes@gmail.com>
2025-06-04 12:48:39 +0000

    final

---
+++ .vscode/Tema2Praticando5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int idadeManu = 45;
+    int idadePaula = 38;
+
+    if (idadeManu > idadePaula) {
+        
+        printf("Manu é mais velha que Paula");
+
+
+    }
+
+   
+   
+
+    
+  
+}
