cfb2ae64eda1cf4b55b00c314df3a2acebc7f4cf
manulopoli <manu.lopes@gmail.com>
2025-06-04 13:49:52 +0000

    final

---
+++ .vscode/Tema2Praticando12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 50;
+
+    if (idade >= 18) {
+        
+        printf("Você é maior de idade\n"); //verdadeira
+
+    }else{
+        printf("Você é menor de idade\n"); //falsa
+
+
+    }
+
+       
+}
