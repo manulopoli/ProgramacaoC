8a1901f71367eb95571bb23413dcf17c12e365b6
manulopoli <manu.lopes@gmail.com>
2025-06-04 13:33:41 +0000

    final

---
+++ .vscode/Tema2Praticando9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int numero1;
+    int numero2;
+
+    numero1 = 2;
+    numero2 = 5;
+
+    if(numero1 > numero2) {
+        printf("O número 1 é maior que o número 2\n");
+
+    }else {
+
+        printf("O número 1 é menor que o número 2\n");
+
+    }
+       
+  
+}
