f88b6a947679dfdaf086e48cbcc116dd4b0b8584
manulopoli <manu.lopes@gmail.com>
2025-06-04 12:42:55 +0000

    final

---
+++ .vscode/Tema2Praticando4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int peso = 75;
+
+    if (peso <= 70) {
+
+        printf("Você é gostosa!\n");
+
+        
+    }
+   
+    printf("Você é linda!");
+
+
+
+    
+  
+}
