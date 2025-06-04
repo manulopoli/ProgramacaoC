97f410569d208b26339274419aa9f5b0ff9f3612
manulopoli <manu.lopes@gmail.com>
2025-06-04 13:20:12 +0000

    final

---
+++ .vscode/Tema2Praticando7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int numero = 4, resultado;
+
+    resultado = numero % 2;
+
+    printf("A variável resultado é: %d\n", resultado);
+
+    if (numero % 2 ==0) {
+        printf("O número é par\n");
+
+    }
+     
+  
+}
