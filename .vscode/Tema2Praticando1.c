fe8c6f0b238c005f4eaa7ff5534c0cb2209b2caf
manulopoli <manu.lopes@gmail.com>
2025-06-04 12:16:08 +0000

    final

---
+++ .vscode/Tema2Praticando1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int numero1, numero2;
+
+    numero1 = 10;
+    numero2 = 10;
+
+    if (numero1 > numero2) {
+        printf("Número 1 é maior que número 2\n");
+        }
+
+        printf("Fora IF");
+
+    return 0;
+}
