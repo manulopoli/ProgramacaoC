a5481088af743b3f9aff64f78aec8081d70f676c
manulopoli <manu.lopes@gmail.com>
2025-06-05 14:11:29 +0000

    final

--- Tema3/Tema3Praticando7..c
+++ Tema3/Tema3Praticando7..c
@@ -1,15 +1,11 @@
 #include <stdio.h>
+ 
 int main() {
-   
-    int numero, i;
-
-    printf("Digite um número para calcularmos a tabuada:\n");
-    scanf("%d", &numero);
-
-    for (i = 0; i <= 10; i++)
-    {
-        printf("%d x %d = %d\n", i, numero, i * numero);
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 == 0) {
+            printf("%d\n", i);
+        }
     }
-
+   
     return 0;
 }
