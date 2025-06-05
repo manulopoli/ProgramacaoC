b1db01de09b22c3b9a366244b664e79e1f1a66c4
manulopoli <manu.lopes@gmail.com>
2025-06-05 16:54:18 +0000

    final

--- Tema3/Tema3Praticando8.c
+++ Tema3/Tema3Praticando8.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
  
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
-            printf("%d\t", i * j);
-        }
-        printf("\n");
+    for (int i = 0, j = 10; i < j; i++, j--) {
+        printf("i = %d, j = %d\n", i, j);
     }
     return 0;
 }
